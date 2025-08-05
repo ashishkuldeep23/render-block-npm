e6c3ad8d8b939fd91b56d2737831d4747cb5952c
Ashish kuldeep <ashishkuldeep6@gmail.com>
2025-08-05 23:57:21 +0530

    Code updated

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import RenderBlockMain from "./components/RenderBlocks/RenderBlockMain";
 
 import HelloWorld from "./components/HelloWorld/HelloWord";
 
-
 export default RenderBlockMain;
 export { HelloWorld };
